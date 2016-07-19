c9400819794652f170bfefa13d6886f42a22fe75
bnbshenhui <shenhui@11bnb.com>
2016-07-19 19:20:32 +0800

    markdown example

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-var express = require('express')
-var app = express()
+var escapeHtml = require('escape-html');
+var express = require('express');
+var fs = require('fs');
+var marked = require('marked');
 
-  app.get('/', function(req, res){
-    res.send('Hello World')
+var app = module.exports = express();
+
+  app.engine('md', function(path, options, fn){
+    fs.readFile(path, 'utf8', function(err, str){
+      if(err) return fn(err);
+      var html = marked.parse(str).replace(/\{[^}]+)\}/g,function(_, name){
+        return escapeHtml(options[name] || '');
+      });
+    fn(null, html);
+    })
   })
 
-app.listen(process.env.PORT || 5050)
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'md')
+
+app.get('/', function(req, res){
+  res.render('index', {title : 'Markdown example'});
+});
+
+app.get('/fail', function(req, res){
+  res.render('missing', {title : 'Markdown Example'})
+});
+
+if(!module.parent){
+  app.listen(3000);
+  console.log("Express start at 3000");
+}
